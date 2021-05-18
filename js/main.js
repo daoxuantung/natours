d62da744a18a051474eb228caa631b987cdda12a
leooblivion <leooblivion24@gmail.com>
2021-05-18 19:33:09 +0700

    Building navigation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   const form = document.getElementById("form");
-  const name = document.getElementById("name");
-  const email = document.getElementById("email");
-
-  name.addEventListener("blur", (e) => {
-    name.style.borderColor = "transperant";
-  });
-
+  const toggle = document.getElementById("toggle");
+  const background = document.getElementById("background");
+  const nav = document.getElementById("nav");
+  const stick1 = document.getElementById("stick-1");
+  const stick2 = document.getElementById("stick-2");
+  const stick3 = document.getElementById("stick-3");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+  });
+
+  toggle.addEventListener("click", () => {
+    stick1.classList.toggle("rotate");
+    stick2.classList.toggle("transparent");
+    stick3.classList.toggle("rotate-reverse");
+    background.classList.toggle("show");
+    nav.classList.toggle("isShow");
   });
 });
