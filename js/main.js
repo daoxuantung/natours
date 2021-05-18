3f57387052323099414299224e9dd98a2061aedd
leooblivion <leooblivion24@gmail.com>
2021-05-18 16:20:49 +0700

    Buiding the story section and footer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  const form = document.getElementById("form");
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
 
+  name.addEventListener("blur", (e) => {
+    name.style.borderColor = "transperant";
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+});
